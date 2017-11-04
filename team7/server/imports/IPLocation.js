81c659a28cd039373ff37be2ffc1f461c61fb5c5
Pitchaya Boonsarngsuk <brianboon@disroot.org>
2017-11-04 17:51:43 +0000

    Missing a semicolon #SomethingStupid

--- team7/server/imports/IPLocation.js
+++ team7/server/imports/IPLocation.js
@@ -2,7 +2,7 @@ export { IPLocation };
 
 /*
 Usage:
-import { IPLocation } from '/server/imports/IPLocation.js'
+import { IPLocation } from '/server/imports/IPLocation.js';
 IPLocation.lookupCountry('130.209.241.197'); //returns "GB"
 
 Not very accurate but good enough for this case.
