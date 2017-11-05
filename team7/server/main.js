f59f6995f58055e804c401b0d684f4563769e73a
Pitchaya Boonsarngsuk <brianboon@disroot.org>
2017-11-05 10:33:20 +0000

    [Server] Seperate external data fetcher

--- team7/server/main.js
+++ team7/server/main.js
@@ -1,54 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
+import { dataFetcher } from '../server/imports/fetchDataSources.js';
 
 import './methods.js';
 
-const TOPICS = ['SL.TLF.TOTL.FE.ZS', 'SG.VAW.1549.ZS', 'SE.SEC.ENRL.FE.ZS'];
-const LIMIT = 10000;
-const YEAR_RANGE = 10;
-const YEAR_L = new Date().getFullYear() - (1 + YEAR_RANGE);
-const YEAR_U = new Date().getFullYear() - 1;
-
-let DATA = {};
-
 Meteor.startup(() => {
     // code to run on server at startup
-    //fetchData();
+    dataFetcher.fetch();
 });
-
-function fetchData() {
-    try {
-        for (let i = 0; i < TOPICS.length; i++) {
-            const response = HTTP.get(`http://api.worldbank.org/countries/indicators/${TOPICS[i]}?format=json&per_page=${LIMIT}&date=${YEAR_L}:${YEAR_U}`);
-            const parseResponse = JSON.parse(response.content);
-            const result = {status: response.statusCode, meta: parseResponse[0], content: removeNullValues(parseResponse[1])};
-            formatData(TOPICS[i], result.content);
-            // console.log(result);
-        }
-        console.log(DATA);
-    } catch (e) {
-        console.log(e);
-    }
-}
-
-function removeNullValues(array) {
-    let formattedArray = [];
-    const size = array.length;
-    for (let i = 0; i < size; i++) {
-        if (array[i].value !== null) {
-            formattedArray.push(array[i]);
-        }
-    }
-    return formattedArray;
-}
-
-function formatData(topic, array) {
-    const size = array.length;
-    let data = {};
-    for (let i = 0; i < size; ++i) {
-        const countryId = array[i].country.id;
-        if (!(countryId in data)) {data[countryId] = {};}
-        data[countryId][array[i].date] = array[i].value;
-    }
-    DATA[topic] = data;
-}
