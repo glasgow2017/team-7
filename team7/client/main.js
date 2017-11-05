01c973298bad042b2f189aa9111bf1323f8bf579
Aaron Cheng <aaron.cheng@live.co.uk>
2017-11-05 00:21:23 +0000

    Added method to get public data set
    - Import http package
    - fetches topic data
    - create method to remove null values
    - call meteor methods from client

--- team7/client/main.js
+++ team7/client/main.js
@@ -1,19 +1,20 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+let QUESTIONS;
+
 // ============== Route ======================//
-FlowRouter.route('/',{
+FlowRouter.route('/', {
     name: 'home',
-    action(params, queryParams){
+    action(params, queryParams) {
         BlazeLayout.render('home');
     }
 });
 
-FlowRouter.route('/quiz',{
+FlowRouter.route('/quiz', {
     name: 'quiz',
-    action(params, queryParams){
+    action(params, queryParams) {
         BlazeLayout.render('basic_info');
     }
 });
@@ -21,26 +22,44 @@ FlowRouter.route('/quiz',{
 // =========== Event Handling ===============//
 Template.home.events({
     'submit .goto-basic-info'(event) {
-      // Prevent default browser form submit
-      event.preventDefault();
+        // Prevent default browser form submit
+        event.preventDefault();
 
-      // Get value from form element
-      FlowRouter.go('/quiz');
+        //Get client location based on IP
+        //   Meteor.call('getClientIPCountry', (err, result) => {
+        //       if(!err) {
+        //           console.log(result === 'Unknown' ? 'n/a' : result);
+        //       }
+        //   });
+
+        // Get value from form element
+        FlowRouter.go('/quiz');
     },
 });
 
 Template.basic_info.events({
-  'submit .submit-basic-info'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
+    'submit .submit-basic-info'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
 
-    // Get value from form element
-    console.log(event.target);
-    var ageGroup = event.target.inputAge.value;
-    var gender = event.target.genderRadios.value;
-    // store the variable somewhere
-    BlazeLayout.render('quiz');
-    console.log(ageGroup);
-    console.log(gender);
-  },
+        // Get value from form element
+        const ageGroup = event.target.inputAge.value;
+        const gender = event.target.genderRadios.value;
+        // store the variable somewhere
+
+        console.log(ageGroup);
+        console.log(gender);
+
+        // Fetch questions from server
+        Meteor.call('getQuestions', (err, result) => {
+            if (!err) {
+                QUESTIONS = result;
+                console.log(QUESTIONS);
+                BlazeLayout.render('quiz');
+            }
+            else {
+                alert('Error fetching questions from server');
+            }
+        });
+    },
 });
--- team7/server/main.js
+++ team7/server/main.js
@@ -1,6 +1,33 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+
 import './methods.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
+    fetchData();
 });
+
+function fetchData() {
+    let result;
+    try {
+        const response = HTTP.get('http://api.worldbank.org/countries/indicators/SL.TLF.TOTL.FE.ZS?format=json&per_page=20&date=2006:2016');
+        //response = JSON.parse(response);
+        const data = JSON.parse(response.content);
+        result = {status: response.statusCode, meta: data[0], content: removeNullValues(data[1])};
+        console.log(result);
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+function removeNullValues(array) {
+    let formattedArray = [];
+    const size = array.length;
+    for (let i = 0; i < size; i++) {
+        if(array[i].value !== null) {
+            formattedArray.push(array[i]);
+        }
+    }
+    return formattedArray;
+}
