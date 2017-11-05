7f28cd55ccec397381535b56849e4150893f02f5
Aaron Cheng <aaron.cheng@live.co.uk>
2017-11-05 07:57:41 +0000

    Fetch and store data on the required topics

--- team7/server/main.js
+++ team7/server/main.js
@@ -3,23 +3,31 @@ import { HTTP } from 'meteor/http';
 
 import './methods.js';
 
-let data = {};
+const TOPICS = ['SL.TLF.TOTL.FE.ZS', 'SG.VAW.1549.ZS', 'SE.SEC.ENRL.FE.ZS'];
+const LIMIT = 10;
+const YEAR_RANGE = 10;
+const YEAR_L = new Date().getFullYear() - (1 + YEAR_RANGE);
+const YEAR_U = new Date().getFullYear() - 1;
+
+let DATA = {};
+
 Meteor.startup(() => {
     // code to run on server at startup
+    fetchData();
 });
 
 function fetchData() {
     let result;
     try {
-        // female work force percentage
-        const currentYear = new Date().getFullYear();
-        const response = HTTP.get('http://api.worldbank.org/countries/indicators/SL.TLF.TOTL.FE.ZS?format=json&per_page=10000&date=' + (currentYear - 11) + ':' + (currentYear - 1));
-        //response = JSON.parse(response);
-        const data = JSON.parse(response.content);
-        result = {status: response.statusCode, meta: data[0], content: removeNullValues(data[1])};
-        fomatData(result.content);
-        // console.log(result);
-    } catch(e) {
+        for (let i = 0; i < TOPICS.length; i++) {
+            const response = HTTP.get(`http://api.worldbank.org/countries/indicators/${TOPICS[i]}?format=json&per_page=${LIMIT}&date=${YEAR_L}:${YEAR_U}`);
+            const parseResponse = JSON.parse(response.content);
+            result = {status: response.statusCode, meta: parseResponse[0], content: removeNullValues(parseResponse[1])};
+            formatData(TOPICS[i], result.content);
+            // console.log(result);
+        }
+        console.log(DATA);
+    } catch (e) {
         console.log(e);
     }
 }
@@ -28,23 +36,20 @@ function removeNullValues(array) {
     let formattedArray = [];
     const size = array.length;
     for (let i = 0; i < size; i++) {
-        if(array[i].value !== null) {
+        if (array[i].value !== null) {
             formattedArray.push(array[i]);
         }
     }
     return formattedArray;
 }
 
-function fomatData(array){
+function formatData(topic, array) {
     const size = array.length;
-    for (let i = 0; i < size; ++i){
+    let data = {};
+    for (let i = 0; i < size; ++i) {
         const countryId = array[i].country.id;
-        if (!(countryId in data)){
-            data[countryId] = {};
-        }
-        const date = array[i].date;
-        const val = array[i].value;
-        data[countryId][date] = val;
+        if (!(countryId in data)) {data[countryId] = {};}
+        data[countryId][array[i].date] = array[i].value;
     }
-
+    DATA[topic] = data;
 }
